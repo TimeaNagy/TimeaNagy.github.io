6ff5564cba0e59111bca5862acd2e008b4453da9
Tóth Krisztian <ktoth@gratex.com>
2022-10-15 18:12:10 +0200

    I hate swiper

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { fromEvent, map, Observable, Observer, take } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
-import SwiperCore, { Pagination, Navigation, Grid, SwiperOptions, Swiper } from "swiper";
+import SwiperCore, { Pagination, Navigation, Grid, SwiperOptions } from "swiper";
 import { GalleryDialogComponent } from './dialog/gallery-dialog/gallery-dialog.component';
 import { SwiperComponent } from 'swiper/angular';
 SwiperCore.use([Pagination, Navigation, Grid]);
@@ -25,18 +25,30 @@ interface ISize {
   encapsulation: ViewEncapsulation.None,
 })
 export class GalleryComponent {
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+
   asyncTabs: Observable<GalleryTab[]>;
   defaultTabIndex = 0;
   tiles: Tile[] = [];
-  @ViewChild(SwiperComponent) swiper: SwiperComponent | undefined;
+
   config: SwiperOptions = {
     slidesPerView: 3,
     slidesPerGroup: 3,
     grid: { rows: 2, fill: 'row' },
     spaceBetween: 15,
     grabCursor: true,
-    pagination: { clickable: true, dynamicBullets: true }
+    pagination: { clickable: true, dynamicBullets: true },
+    observer: true,
+    observeParents: true
   };
+
+  folderInfo = [
+    { name: 'bouquet', pictureCount: 99 },
+    { name: 'wreath', pictureCount: 71 },
+    { name: 'ikebana', pictureCount: 22 },
+    { name: 'decoration', pictureCount: 46 },
+    { name: 'box', pictureCount: 58 }
+  ];
 
   constructor(private translate: TranslateService, public dialog: MatDialog) {
     this.asyncTabs = new Observable((observer: Observer<GalleryTab[]>) => {
@@ -58,16 +70,11 @@ export class GalleryComponent {
 
   selectedTabChanged(event: any) {
     this.tiles = [];
-    const folderInfo = [
-      { name: 'bouquet', pictureCount: 99 },
-      { name: 'wreath', pictureCount: 72 },
-      { name: 'ikebana', pictureCount: 22 },
-      { name: 'decoration', pictureCount: 46 },
-      { name: 'box', pictureCount: 58 }
-    ];
 
-    for (var i = 1; i <= folderInfo[event.index].pictureCount; i++) {
-      this.tiles.push({ src: `../../assets/images/${folderInfo[event.index].name}/${folderInfo[event.index].name}${i}.jpg` })
+    console.log(this.swiper?.swiperRef.slides.length)
+    for (var i = 1; i <= this.folderInfo[event.index].pictureCount; i++) {
+      this.tiles = [...this.tiles, { src: `../../assets/images/${this.folderInfo[event.index].name}/${this.folderInfo[event.index].name}${i}.jpg` }];
+      this.swiper?.swiperRef.updateSlides();
     }
   }
 
